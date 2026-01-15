2699a77516fbd28f8f6ca287849b46802fb1d42d
travisdotdev <travis4yusuf@gmail.com>
2026-01-15 14:31:36 +0000

    Tests for TextNode Class

---
+++ src/test_textnode.py
@@ -0,0 +1,46 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a textnode", TextType.BOLD)
+        node2 = TextNode("This is a textnode", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("This is a textnode", TextType.BOLD)
+        node2 = TextNode("This is a different textnode", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_url_is_none(self):
+        node = TextNode("This is a textnode", TextType.BOLD)
+        self.assertIsNone(node.url)
+
+    def test_text_is_string(self):
+        node = TextNode("String", TextType.TEXT)
+        self.assertTrue(isinstance(node.text, str))
+
+    def test_nodes_equal_same_values_with_url(self):
+        node1 = TextNode("String", TextType.TEXT, "https://example.com")
+        node2 = TextNode("String", TextType.TEXT, "https://example.com")
+        self.assertEqual(node1, node2)
+
+    def test_text_type_not_eq(self):
+        node = TextNode("String", TextType.TEXT)
+        node2 = TextNode("String", TextType.BOLD)
+        self.assertNotEqual(node.text_type.value, node2.text_type.value)
+
+    def test_nodes_not_equal_when_url_differs(self):
+        node1 = TextNode("String", TextType.TEXT, "https://a.com")
+        node2 = TextNode("String", TextType.TEXT, "https://b.com")
+        self.assertNotEqual(node1, node2)
+
+    def test_repr(self):
+        node = TextNode("String", TextType.TEXT, "https://a.com")
+        self.assertEqual("TextNode(String, text, https://a.com)", repr(node))
+
+
+if __name__ == "__main__":
+    unittest.main()
