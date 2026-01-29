4f9ac13d1f3474d60e02a6f3dbee735706e6f3fc
travisdotdev <travis4yusuf@gmail.com>
2026-01-29 14:17:35 +0000

    Added text to image and link node conversion + a few tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -31,3 +31,53 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     extracted_links = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return extracted_links
+
+
+def split_nodes_image(old_nodes):
+    regex_extracted_nodes = []
+    new_nodes = []
+    original_text = ""
+    for node in old_nodes:
+        regex_extracted_nodes = extract_markdown_images(node.text)
+        if not regex_extracted_nodes:
+            new_nodes.append(node)
+        else:
+            original_text = node.text
+            split_text = []
+            for image in regex_extracted_nodes:
+                split_text = original_text.split(f"![{image[0]}]({image[1]})", 1)
+                if split_text[0]:
+                    new_node = TextNode(split_text[0], TextType.TEXT)
+                    new_nodes.append(new_node)
+                new_node = TextNode(image[0], TextType.IMAGE, image[1])
+                new_nodes.append(new_node)
+                original_text = split_text[1]
+            if original_text:
+                new_node = TextNode(original_text, TextType.TEXT)
+                new_nodes.append(new_node)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    regex_extracted_nodes = []
+    new_nodes = []
+    original_text = ""
+    for node in old_nodes:
+        regex_extracted_nodes = extract_markdown_links(node.text)
+        if not regex_extracted_nodes:
+            new_nodes.append(node)
+        else:
+            original_text = node.text
+            split_text = []
+            for link in regex_extracted_nodes:
+                split_text = original_text.split(f"[{link[0]}]({link[1]})", 1)
+                if split_text[0]:
+                    new_node = TextNode(split_text[0], TextType.TEXT)
+                    new_nodes.append(new_node)
+                new_node = TextNode(link[0], TextType.LINK, link[1])
+                new_nodes.append(new_node)
+                original_text = split_text[1]
+        if original_text:
+            new_node = TextNode(original_text, TextType.TEXT)
+            new_nodes.append(new_node)
+    return new_nodes
