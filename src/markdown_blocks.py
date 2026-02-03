013808c98af8f27c363550a98006be713875fd54
travisdotdev <travis4yusuf@gmail.com>
2026-02-03 13:08:50 +0000

    finished block type checking function, added some tests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,15 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +19,45 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    split_block = block.strip().split(" ", 1)
+    is_heading = (
+        len(split_block) == 2
+        and 1 <= len(split_block[0]) <= 6
+        and all(char == "#" for char in split_block[0])
+        and split_block[1].strip != ""
+    )
+    if is_heading:
+        return BlockType.HEADING
+    is_code = block.startswith("```\n") and block.endswith("```")
+    if is_code:
+        return BlockType.CODE
+    split_block = block.strip().split("\n")
+    is_quote = [line.startswith(">") for line in split_block]
+    if all(is_quote):
+        return BlockType.QUOTE
+    split_block = block.strip().split("\n")
+    is_unordered_list = [line.startswith("- ") for line in split_block]
+    if all(is_unordered_list):
+        return BlockType.UNORDERED_LIST
+    split_block = block.strip().split("\n")
+    split_block2 = []
+    fullstop_present = False
+    incrementing_by_one = False
+    incrementing_list = []
+    for line in split_block:
+        split_block2.append(line.strip().split(" ", 1))
+    for line2 in split_block2:
+        if not line2[0].endswith("."):
+            fullstop_present = False
+        else:
+            fullstop_present = True
+        incrementing_list.append(int(line2[0].strip(".")))
+    if incrementing_list == list(range(1, len(incrementing_list) + 1)):
+        incrementing_by_one = True
+    is_ordered_list = incrementing_by_one and fullstop_present
+    if is_ordered_list:
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestMarkdownBlocks(unittest.TestCase):
@@ -42,6 +42,69 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_blocktype_ordered_list(self):
+        block = """
+1. hey hey hey
+2. right hello Hi
+3. Greetings bonjour23910
+4. Hello
+"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+    def test_blocktype_invalid_ordered_list(self):
+        block = """
+1. hey hey hey
+2. right hello Hi
+31. Greetings bonjour23910
+4. Hello
+"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_blocktype_heading(self):
+        block = """### HEADING"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_blocktype_code(self):
+        block = """```
+print(this_is_code)
+```"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_blocktype_quote(self):
+        block = """
+>these are quotes
+>hello world
+>testing testing
+> for i am become death destroyer of worlds -Vishnu 
+"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_blocktype_unordered_list(self):
+        block = """
+- these are quotes
+- hello world
+- testing testing
+- for i am become death destroyer of worlds -Vishnu 
+"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.UNORDERED_LIST)
+
+        #     def test_blocktype_paragraph(self):
+        #         block = """
+        # 1.HELLO
+        # 3.greetings
+        # - wow
+        # > neo
+        # ````
+        # """
+        # result = block_to_block_type(block)
+        # self.assertEqual(result, BlockType.PARAGRAPH)
 
 
 if __name__ == "__main__":
