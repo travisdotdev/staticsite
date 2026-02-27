8bda7fdecd1fa730840d5b17f3a39583999a7737
travisdotdev <travis4yusuf@gmail.com>
2026-02-27 13:23:00 +0000

    Markdown -> HTML static site generator working for single page, added page generation, reconfigured main, added line to run server in .sh file

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,7 +1,7 @@
-content_path = "content/index.md"
+import os
+from markdown_blocks import markdown_to_html_node
 
-with open(content_path, "r", encoding="utf-8") as f:
-    md = f.read()
+content_path = "content/index.md"
 
 
 def extract_title(markdown):
@@ -10,3 +10,21 @@ def extract_title(markdown):
         if line.startswith("# "):
             return line[2:].strip()
     raise ValueError("Title not found")
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r", encoding="utf-8") as f:
+        markdown = f.read()
+    title = extract_title(markdown)
+    md_to_html = markdown_to_html_node(markdown)
+    with open(template_path, "r", encoding="utf-8") as f2:
+        html_file = f2.read()
+    html_file = html_file.replace("{{ Title }}", title)
+    html_file = html_file.replace("{{ Content }}", md_to_html.to_html())
+    parent = os.path.dirname(dest_path)
+    if parent:
+        os.makedirs(parent, exist_ok=True)
+    with open(dest_path, "w", encoding="utf-8") as f:
+        f.write(html_file)
+    print("HTML File Created")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from copystatic import copy_files_recursive, clean_up_structure
+from gencontent import extract_title, generate_page
 
 STATIC_PATH = "./static"
 DESTINATION_PATH = "./public"
+DESTINATION_PATH_HTML = "./public/index.html"
+HTML_TEMPLATE_PATH = "./template.html"
+MARKDOWN_PATH = "./content/index.md"
 
 
 def main():
     clean_up_structure(DESTINATION_PATH)
     copy_files_recursive(STATIC_PATH, DESTINATION_PATH)
+    generate_page(MARKDOWN_PATH, HTML_TEMPLATE_PATH, DESTINATION_PATH_HTML)
 
 
 if __name__ == "__main__":
